aa2d9d7d4402b15c2514db4840526f6eb4196431
Leonardo <hgtleohgt@gmail.com>
2022-07-26 14:30:36 -0300

    BarbosaALeonardoHenriqueExercicioA009

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nomeUsuario = prompt("Digite seu nome:")
+const comidaFavorita1 = prompt("Digite sua comida favorita:")
+const comidaFavorita2 = prompt("Digite outra comida favorita:")
+const comidaFavorita3 = prompt("Digite mais uma comida favorita:")
+
+console.log(`Estas são as comidas favoritas do/da${nomeUsuario}\n-${comidaFavorita1}\n-${comidaFavorita2}\n-${comidaFavorita3}`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+console.log(`A primeira string tem ${minhaString.length} caracteres!`)
+const novaString = "Eu não sou supersticioso, mas sou um pouco ________.";
+console.log(novaString.trim())
+console.log(`A segunda string tem ${novaString.length} caracteres!`)
+
+const fraseFormada = novaString.replace("________","sticioso ")
+console.log((fraseFormada))
